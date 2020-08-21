5c2e12bf3bdae37fc744670a8ec8cbea779011f5
Thales Gomes <thales3238junior@gmail.com>
2020-08-21 18:54:18 -0400

    Conclusão das Controllers de Guardar e Resgatar dinheiro. (21.08.2020)

--- WeBank.API/DTOs/UserBalanceDTO.cs
+++ WeBank.API/DTOs/UserBalanceDTO.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
+
 namespace WeBank.API.DTOs
 {
     public class UserBalanceDTO
     {
         public decimal Balance { get; set; }
         public decimal SavedBalance { get; set; }
+        public List<ExtractDTO> Extracts { get; set; }
         
     }
 }
